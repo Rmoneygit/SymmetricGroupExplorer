70847a78210401d2a0eb4756a0048bc49a2f2476
Ryan Patrick <rmoneymail5@gmail.com>
2025-08-31 10:51:07 -0700

    CMakeLists.txt can now find Flex and Bison.

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,11 @@
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout
 import os
 
 class SymmetricGroupExplorerConan(ConanFile):
     name = "SymmetricGroupExplorer"
     version = "1.0"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
 
     # Specify dependencies here
     requires = (
@@ -16,7 +14,10 @@ class SymmetricGroupExplorerConan(ConanFile):
         "winflexbison/2.5.25"
     )
 
-    def toolchain(self):
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+
         tc = CMakeToolchain(self)
         winflexbison = self.dependencies["winflexbison"]
         bin_dir = winflexbison.cpp_info.bindirs[0]
@@ -26,13 +27,12 @@ class SymmetricGroupExplorerConan(ConanFile):
         tc.cache_variables["FLEX_EXECUTABLE"] = flexJoinedPath
 
         bisonJoinedPath = os.path.join(bin_dir, "win_bison.exe")
-        print("Setting FLEX_EXECUTABLE=" + bisonJoinedPath)
+        print("Setting BISON_EXECUTABLE=" + bisonJoinedPath)
         tc.cache_variables["BISON_EXECUTABLE"] = bisonJoinedPath
 
-        return tc
+        tc.generate()
 
     def layout(self):
         cmake_layout(self)
-        #self.folders.generators = "."
         print("Build folder: " + self.folders.build)
         print("Generators folder: " + self.folders.generators)
